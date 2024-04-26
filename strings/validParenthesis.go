df5a05313f6506711bdfc6e3ab75828160502e82
samar <scipioresearch2020@gmail.com>
2024-04-26 14:36:26 +0530

    in

--- strings/validParenthesis.go
+++ strings/validParenthesis.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func isValid(s string) bool {
 	stack := []string{}
 	elMap := map[string]struct{}{
@@ -40,10 +42,58 @@ func isValid(s string) bool {
 	return false
 }
 
+func isValidWithAsterisk(s string) bool {
+	stack := []string{}
+	elMap := map[string]struct{}{
+		"[": {},
+		"{": {},
+		"(": {},
+	}
+	for _, el := range s {
+		els := string(el)
+		if _, ok := elMap[els]; ok {
+			stack = append(stack, els)
+		}
+		if els == "]" {
+			if stack[len(stack)-1] == "[" || stack[len(stack)-1] == "*" {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		}
+		if els == "}" {
+			if stack[len(stack)-1] == "{" || stack[len(stack)-1] == "*" {
+				stack = stack[:len(stack)-1]
+
+			} else {
+				return false
+			}
+		}
+		if els == ")" {
+			if stack[len(stack)-1] == "(" || stack[len(stack)-1] == "*" {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		}
+		if els == "*" {
+			fmt.Println(stack)
+			if _, ok := elMap[stack[len(stack)-1]]; ok {
+				stack = stack[:len(stack)-1]
+			} else {
+				stack = append(stack, els)
+			}
+
+		}
+	}
+	return len(stack) == 0
+}
+
 func main() {
 	println(isValid("()"))
 	println(isValid("()[]{}"))
 	println(isValid("(]"))
 	println(isValid("([)]"))
 	println(isValid("{[]}"))
+	println(isValidWithAsterisk("[(*){*)]"))
 }
